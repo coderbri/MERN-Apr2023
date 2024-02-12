68021dd79128f5bc98cabdc6eb741fb8255b6897
coderbri <coderbri23@gmail.com>
2024-02-12 02:16:34 -0500

    added a jwt.config.js file // not used but was created for learning purposes

--- Wk7-MERN_Auth/Lecture-Code/D22-Login_and_Registration_Version_2/server/config/mongoose.config.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(`mongodb://localhost:27017/login-registration-practice`)
-    .then(() => console.log("Established a connection to the database."))
-    .catch((error) => console.log("Something went wrong when connecting to the database", error));
--- Wk7-MERN_Auth/Lecture-Code/D22-Login_and_Registration_Version_2/server/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const UserController = require('../controllers/user.controller');
-
-module.exports = app => {
-    app.post("/api/register/user", UserController.registerUser);
-    app.post("/api/login/user", UserController.loginUser);
-    app.post("/api/logout/user", UserController.logoutUser);
-}
